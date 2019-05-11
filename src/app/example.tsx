3d862edc308487484ee051bde4c2406bfd30f6e7
Mario Eismayer <mario.eismayer@gmail.com>
2019-05-11 19:45:07 +0200

    show list issue

--- src/app/example.tsx
+++ src/app/example.tsx
@@ -2,9 +2,16 @@ import * as React from "react";
 import { FunctionComponent } from "react";
 import Paper from "@material-ui/core/Paper";
 import styled from "styled-components";
+import { List } from "@material-ui/core";
+import ListItemText from "@material-ui/core/ListItemText";
 
 const StyledPaper = styled(Paper)`
     background-color: green;
+    text-align: center;
+`;
+
+const StyledList = styled(List)`
+    background-color: blue;
     text-align: center;
 `;
 
@@ -12,5 +19,17 @@ export const Example: FunctionComponent = () => {
     return <div>
         <StyledPaper>Hello Styled World!</StyledPaper>
         <Paper>Hello Plain World!</Paper>
+
+        <div style={{marginBottom: "100px"}}/>
+
+        <StyledList>
+            <ListItemText>Styled List without attributes works</ListItemText>
+        </StyledList>
+        <StyledList component="nav">
+            <ListItemText>Styled List with attributes doesn't work</ListItemText>
+        </StyledList>
+        <List component="nav">
+            <ListItemText>Plain List with attributes works</ListItemText>
+        </List>
     </div>;
 };
